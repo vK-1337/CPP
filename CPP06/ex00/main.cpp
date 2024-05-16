e20ebafd8bb1e9f350826fabf71592e6379d7ab0
Vincent Da Conceicao Ramos <vda-conc@bess-f2r4s16.clusters.42paris.fr>
2024-05-16 11:36:35 +0200

    Pushing cpp06

---
+++ CPP06/ex00/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string myString = "Hello, world!";
+    std::cout << myString << std::endl; // Output: Hello, world!
+    return 0;
+}
+
+"Coucou";
+"e";
+"10";
+"1.0f";
+"1.0";
+
+"e"
+"101"
+"101.0f"
+"101.0"
